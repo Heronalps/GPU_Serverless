1c4647aaf58ab59d9f5aabb2d51cebbec912de0f
Michael Zhang <heronalps@gmail.com>
2019-03-28 14:33:38 -0700

    mz - update gpu-test.py

--- kubeless/deploy.sh
+++ kubeless/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-kubeless function deploy hello --runtime python3.6 \
-                                --from-file hello.py \
-                                --handler hello.hello
+# kubeless function deploy hello --runtime python3.6 \
+#                                 --from-file hello.py \
+#                                 --handler hello.hello
+
+kubeless function deploy gpu-test --runtime gpupython3.7 \
+                                  --from-file gpu-test.py \
+                                  --handler gpu-test.handler
