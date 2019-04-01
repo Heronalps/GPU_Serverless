e13cc72e79c9da6a6b4b0eea7ebcbc682d0d9bd6
Michael Zhang <heronalps@gmail.com>
2019-03-31 18:59:17 -0700

    mz - update uec-food func.py

--- iron-io/uec_food_func/func.py
+++ iron-io/uec_food_func/func.py
@@ -11,7 +11,7 @@ for dir_name, _subdir_list, file_list in os.walk(root_dir):
     for file_name in file_list:
         src_file_path = "{0}/{1}".format(dir_name, file_name)
         dest_file_path = src_file_path.replace("UECFOOD100", "UECFOOD100_blur")
-        command_line = "python3 filter.py {0} {1}".format(src_file_path, dest_file_path)
+        command_line = "python3 filter.py {0} {1} -bf".format(src_file_path, dest_file_path)
         proc = Popen(shlex.split(command_line), stdout=PIPE)
         output, _err = proc.communicate()
         print ("=====")
