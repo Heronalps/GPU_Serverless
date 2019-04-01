0e6227c098a2a5b54b67d70f1442728ffb6c8195
Michael Zhang <heronalps@gmail.com>
2019-03-31 18:43:23 -0700

    mz - Add uec-food

---
+++ docs/uec-food.md
@@ -0,0 +1,4 @@
+``` docker run --rm --runtime=nvidia -it -v "$PWD":/img-filter -w /img-filter izone/pycuda /bin/bash ```
+
+
+
---
+++ iron-io/uec_food_func/func.py
@@ -0,0 +1,18 @@
+import os, shlex
+from subprocess import Popen, PIPE
+
+# traverse UECFOOD100
+
+root_dir = './UECFOOD100'
+
+# run python3 filter.py <src> ./UECFOOD100_filter/<dest>
+
+for dir_name, _subdir_list, file_list in os.walk(root_dir):
+    for file_name in file_list:
+        src_file_path = "{0}/{1}".format(dir_name, file_name)
+        dest_file_path = src_file_path.replace("UECFOOD100", "UECFOOD100_blur")
+        command_line = "python3 filter.py {0} {1}".format(src_file_path, dest_file_path)
+        proc = Popen(shlex.split(command_line), stdout=PIPE)
+        output, _err = proc.communicate()
+        print ("=====")
+        print (output)
