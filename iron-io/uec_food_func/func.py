f53cd896b4e7234d12dc1382fb4cea951e8e5644
Michael Zhang <heronalps@gmail.com>
2019-03-31 19:11:41 -0700

    mz - update uec-food dockerfile/func.py

--- iron-io/uec_food_func/func.py
+++ iron-io/uec_food_func/func.py
@@ -9,10 +9,11 @@ root_dir = './UECFOOD100'
 
 for dir_name, _subdir_list, file_list in os.walk(root_dir):
     for file_name in file_list:
-        src_file_path = "{0}/{1}".format(dir_name, file_name)
-        dest_file_path = src_file_path.replace("UECFOOD100", "UECFOOD100_blur")
-        command_line = "python3 filter.py {0} {1} -bf".format(src_file_path, dest_file_path)
-        proc = Popen(shlex.split(command_line), stdout=PIPE)
-        output, _err = proc.communicate()
-        print ("=====")
-        print (output)
+        if file_name.endswith('.jpg'):
+            src_file_path = "{0}/{1}".format(dir_name, file_name)
+            dest_file_path = src_file_path.replace("UECFOOD100", "UECFOOD100_filter")
+            command_line = "python3 filter.py {0} {1} -bf".format(src_file_path, dest_file_path)
+            proc = Popen(shlex.split(command_line), stdout=PIPE)
+            output, _err = proc.communicate()
+            print ("=====")
+            print (output)
