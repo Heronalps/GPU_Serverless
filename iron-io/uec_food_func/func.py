948a2f2d819d79c40d73977833cbab2f67580676
Michael Zhang <heronalps@gmail.com>
2019-03-31 20:04:01 -0700

    mz - Add payload.json

--- iron-io/uec_food_func/func.py
+++ iron-io/uec_food_func/func.py
@@ -1,13 +1,17 @@
-import os, shlex, time
+import os, shlex, time, sys, json
 from subprocess import Popen, PIPE
 
 # traverse UECFOOD100
+if not os.isatty(sys.stdin.fileno()):
+	obj = json.loads(sys.stdin.read())
+	if obj["total"] != "":
+		TOTAL = obj["total"]
 
 root_dir = './UECFOOD100'
 
 # run python3 filter.py <src> ./UECFOOD100_filter/<dest>
 start = time.time()
-
+count = 1
 for dir_name, _subdir_list, file_list in os.walk(root_dir):
     for file_name in file_list:
         if file_name.endswith('.jpg'):
@@ -17,6 +21,10 @@ for dir_name, _subdir_list, file_list in os.walk(root_dir):
             proc = Popen(shlex.split(command_line), stdout=PIPE)
             output, _err = proc.communicate()
             print ("=====")
+            print ("Add filter to No.{} photo".format(count))
             print (output.decode('utf-8'))
+            count = count + 1
+        if count > TOTAL:
+            break
 
 print ("The UECFOOD100 processing time is {} seconds".format(time.time() - start))
