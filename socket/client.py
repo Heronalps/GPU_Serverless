04f6c1e5cc84c35c0da1ef9edf7e9f08ba3463b1
Michael Zhang <heronalps@gmail.com>
2019-11-18 14:54:45 -0800

    mz - Transfer multiple images in zip file

--- socket/client.py
+++ socket/client.py
@@ -1,24 +1,45 @@
 '''
 The data center client requests images from Raspberry Pi
 '''
-import socket, os, time
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((socket.gethostname(), 5005))
+import socket, os, time, zipfile, re
 
 size = 1024
+port = 5005
 folder = "/Users/michaelzhang/Downloads/image_buffer/"
 
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connected = False
 
-fname = "image.jpg"
-path = folder + fname
-fp = open(path, 'wb+')
-while True:
-    strng = client_socket.recv(size)
-    if not strng:
+while not connected:
+    try:
+        client_socket.connect((socket.gethostname(), port))
+        print (f'port : {port}')
+        print ("Connected successfully!")
+        connected = True
+    except ConnectionRefusedError as err:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        port += 1
+        print (f"Increment port : {port}")
+        time.sleep(1)
+    except OSError as e:
+        print (e)
         break
-    fp.write(strng)
 
-fp.close()
+file_name = client_socket.recv(size).decode()
+print (f'file_name : {file_name}')
+zip_name = folder + file_name
+with open(zip_name, 'wb+') as fp:
+    while True:
+        strng = client_socket.recv(size)
+        if not strng:
+            break
+        fp.write(strng)
+
 print ("Data Received successfully")
+
+with zipfile.ZipFile(zip_name, 'r') as f:
+    f.extractall(folder)
+
+os.remove(zip_name)
+client_socket.close()
 exit()
--- socket/server.py
+++ socket/server.py
@@ -2,15 +2,23 @@
 The Raspberry Pi that sends batch of images to data center
 '''
 import os, sys, time, socket, zipfile
-'''
-Constants
-'''
+
+# Constants
 port = 5005
 size = 1024
 folder = '/Users/michaelzhang/Downloads/Github/GPU_Serverless/data/SantaCruzIsland_Labeled_5Class/Birds/test_image/'
+zip_name = 'images.zip'
+connected = False
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((socket.gethostname(), port))
+
+while not connected:
+    try:
+        server_socket.bind((socket.gethostname(), port))
+        connected = True
+        print (f'port : {port}')
+    except:
+        port += 1
 server_socket.listen(5)
 
 client_socket, address = server_socket.accept()
@@ -20,18 +28,22 @@ while True:
     files = os.listdir(folder)
     images = [img for img in files if img.endswith('.JPG')]
     print (f'images : {images}')
-    # Send all .jpg files in the folder from client to server
-    for img in images:
-        print (f'image : {img}')
-        
-        file_name = open(folder + img,'rb')
-        while True:
-            strng = file_name.readline(size)
-            if not strng:
-                break
-            client_socket.send(strng)
-        file_name.close()
-        print (f'{img} is sent successfully !')
+    # Send all .jpg files in the folder from server to client
+    with zipfile.ZipFile(zip_name, 'w') as fp:
+        for img in images:
+            print (f'image : {img}')
+            # Second arg is to specifiy the directory in zip file
+            fp.write(folder + img, "images/" + img)
+            print (f'{img} is zipped successfully !')
+    
+    client_socket.send(zip_name.encode())
+
+    with open(zip_name, 'rb') as f:
+        l = f.read()
+    
+    client_socket.sendall(l)
+    os.remove(zip_name)
     
     print ("All data is sent successfully!")
+    server_socket.close()
     exit()
