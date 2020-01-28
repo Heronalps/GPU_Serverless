898e8c8127de7122c05ce6e8734f9726cc31a7fe
Michael Zhang <heronalps@gmail.com>
2020-01-28 14:27:22 -0800

    mz - finish multiGPU

--- kubeless/image_clf/inference/local_version/image_clf_inf.py
+++ kubeless/image_clf/inference/local_version/image_clf_inf.py
@@ -1,66 +1,123 @@
-from tensorflow.keras.applications.resnet50 import preprocess_input
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import load_model
-from tensorflow.python.client import device_lib
+from multiprocessing import Process, Queue
 import numpy as np
 import time, math, os, argparse
+from gpuinfo import GPUInfo
 
 class_list = ["Birds", "Empty", "Fox", "Humans", "Rodents"]
 NUM_IMAGE = 10
 HOME = os.getenv("HOME")
 INF_DIR = HOME + "/GPU_Serverless/data/SantaCruzIsland_Labeled_5Class/Birds"
+MODEL_DIR = HOME + '/GPU_Serverless/checkpoints/resnet50_model.h5'
 WIDTH = 1920
 HEIGHT = 1080
 
-# PATH = "/racelab/data/SantaCruzIsland_Validation_5Class/Birds/IMG_1304.JPG"
+class Scheduler:
+    def __init__(self, gpu_num):
+        self._queue = Queue()
+        self._gpu_num = gpu_num
+        self.__init_workers()
 
-def handler(event, context): 
-    start1 = time.time()
-    # if isinstance(event['data'], dict) and "path" in event['data']:
-    #     global PATH
-    #     PATH = event['data']['path']
+    def __init_workers(self):
+        self._workers = list()
+        for gpuid in range (self._gpu_num):
+            self._workers.append(Worker(gpuid, self._queue))
 
-    if isinstance(event['data'], dict) and "num_image" in event['data']:
-        global NUM_IMAGE
-        NUM_IMAGE = int(event['data']['num_image'])
+    def start(self, image_list):
+
+        for img in image_list:
+            self._queue.put(img)
+
+        # Add a None to indicate the end of queue
+        self._queue.put(None)
+
+        for worker in self._workers:
+            worker.start()
+
+        for worker in self._workers:
+            worker.join()
+        print ("All image are done inferencing...")
+
+class Worker(Process):
+    def __init__(self, gpuid, queue):
+        Process.__init__(self, name="ModelProcessor")
+        self._gpuid = gpuid
+        self._queue = queue
     
-    # Parallel with multiple GPUs
-    available_devices = device_lib.list_local_devices()
-    NUM_GPU = len([x for x in available_devices if x.device_type == 'GPU'])
-    print ("Current GPU num is {0}".format(NUM_GPU))
+    def run(self):
+        #set enviornment
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(self._gpuid)
 
-    # Increase BATCH_SIZE based on number of GPUs to harness the quasi-linear speedup of multiple GPUS
-    # Each GPU takes 8 augmented images for training at one epoch
-    BATCH_SIZE = 2 * NUM_GPU if NUM_GPU > 0 else 2
+        from tensorflow.keras.applications.resnet50 import preprocess_input
+        from tensorflow.keras.preprocessing import image
+        from tensorflow.keras.models import load_model
+        trained_model = load_model(MODEL_DIR)
+        
+        while True:
+            img_path = self._queue.get()
+            if img_path == None:
+                self._queue.put(None)
+                break
+            img = image.load_img(path=img_path, target_size=(1920, 1080))
+            x = image.img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+            x = preprocess_input(x)
+            y_prob = trained_model.predict(x)
+            index = y_prob.argmax()
+            print ("image : {0}, index : {1}".format(img_path, index))
+        
+        print("GPU {} has done inferencing...".format(self._gpuid))
 
-    # inf_datagen = image.ImageDataGenerator(preprocessing_function=preprocess_input, rotation_range=90, \
-    #                                        horizontal_flip=True, vertical_flip=True)
-    # inf_generator = inf_datagen.flow_from_directory(INF_DIR, target_size=(WIDTH, HEIGHT), \
-    #                                                 batch_size = BATCH_SIZE)
-    
-    trained_model = load_model(HOME + '/GPU_Serverless/checkpoints/resnet50_model.h5')
-    counter = 0
-    start2 = time.time()
+def run_sequential(image_list):
+    from tensorflow.keras.applications.resnet50 import preprocess_input
+    from tensorflow.keras.preprocessing import image
+    from tensorflow.keras.models import load_model
 
-    for img in os.listdir(INF_DIR):
-        PATH = INF_DIR + '/' + img
-        img = image.load_img(path=PATH, target_size=(1920, 1080))
+    trained_model = load_model(MODEL_DIR)
+    for img_path in image_list:
+        img = image.load_img(path=img_path, target_size=(1920, 1080))
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
         y_prob = trained_model.predict(x)
         index = y_prob.argmax()
-        print ("image : {0}, index : {1}".format(PATH, index))
+        print ("image : {0}, index : {1}".format(img_path, index))
+    
+
+def handler(event, context): 
+
+    if isinstance(event['data'], dict) and "num_image" in event['data']:
+        global NUM_IMAGE
+        NUM_IMAGE = int(event['data']['num_image'])
+    
+    # Get GPU counts
+    NUM_GPU = 0
+    available_devices = GPUInfo.check_empty()
+    if available_devices != None:
+        NUM_GPU = len(available_devices)
+    print ("Current GPU num is {0}".format(NUM_GPU))
+    
+    counter = 0
+    image_list = list()
+    for img in os.listdir(INF_DIR):
+        image_list.append(os.path.join(INF_DIR, img))
         counter += 1
         if counter == NUM_IMAGE:
             break
     
-    # y_pred = trained_model.predict_generator(inf_generator, steps = math.ceil(NUM_IMAGE / BATCH_SIZE), workers=8)
-    # print ("shape : ", y_pred.shape)
+    start = time.time()
 
-    # return ("Predicted : {0}  Probability : {1} Time with model loading: {2} Time without model loading {3} for {4} images.".format(class_list[index], y_prob[0][index], (time.time() - start1), (time.time() - start2), NUM_IMAGE))
-    print ("Time with model loading: {0} Time without model loading {1} for {2} images.".format(time.time() - start1, time.time() - start2, NUM_IMAGE))
-    return ("Time with model loading: {0} Time without model loading {1} for {2} images.".format(time.time() - start1, time.time() - start2, NUM_IMAGE))
+    if NUM_GPU == 0:
+        run_sequential(image_list)
+    else:
+        # initialize Scheduler
+        scheduler = Scheduler(NUM_GPU)
+        # start multiprocessing
+        scheduler.start(image_list)
+        
+    end = time.time()
+    # print ("Time with model loading {0} for {1} images.".format(end - start, NUM_IMAGE))
+    return ("Time with model loading {0} for {1} images.".format(end - start, NUM_IMAGE))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
